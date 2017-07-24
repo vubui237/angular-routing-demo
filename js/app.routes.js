f5b367ceb254989e887528574f5b665aa281466b
vubui237 <vtb0977@gmail.com>
2017-07-24 12:26:43 -0500

    STARWZ

--- js/app.routes.js
+++ js/app.routes.js
@@ -1,3 +1,30 @@
-angular.module('routing').config(function() {
+angular.module('routing').config(function($stateProvider, $urlRouterProvider) {
+
+$urlRouterProvider.otherwise('/'); //If they try to access a page that doesn't exist, take them back to the main page.
+
+$stateProvider
+    .state('home',{
+        url: '/',
+        templateUrl: '../views/home.html',
+        controller: 'homeCtrl'
+    })
+    .state('characters', {
+        url: '/characters',
+        templateUrl: '../views/characters.html',
+        controller: 'characterCtrl',
+        resolve: { //This pulls data to be loaded prior to the actual page being loaded. In this case loading characters.
+            characters: function(characterSrvc) {
+                return characterSrvc.getCharacters().then(function(response) {
+                    return response;
+                })
+            }
+        }
+    })
+    .state('details', {
+        url: '/character/:characterId', //This is a state parameter, it's looking for a value. I.E. userName, but in this case characterId, is what will be displayed based on who it is or what char id they clicked on.
+        templateUrl: '../views/details.html',
+        controller: 'detailsCtrl'
+    })
+
 
 });
--- js/details/detailsCtrl.js
+++ js/details/detailsCtrl.js
@@ -1,8 +1,8 @@
-angular.module('routing').controller('detailsCtrl', function($scope, detailsSrvc) {
+angular.module('routing').controller('detailsCtrl', function($scope, $stateParams, detailsSrvc) { //stateparamas pulls from the routing, where we can get character.id from characters.html.
     $scope.getChar = function(charId) {
         detailsSrvc.getChar(charId).then(function(response) {
             $scope.character = response;
         });
     }
-    $scope.getChar();
+    $scope.getChar($stateParams.characterId);
 });
